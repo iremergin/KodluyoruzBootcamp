948837dfae50057537217e03691b48aebdb37dcf
İrem ERGİN <92596689+iremergin@users.noreply.github.com>
2022-03-19 22:20:21 +0300

    Add files via upload

---
+++ KodluyoruzBootcamp/TryCatch/DeltaHesaplama/DeltaHesaplama/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DeltaHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("a: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("b: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("c: ");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+                int delta = b * b - 4 * a * c;
+
+                Console.WriteLine("Delta: " + delta);
+
+            }
+            catch (System.FormatException)
+            {
+
+                Console.WriteLine("Please enter numeric value");
+      
+                
+            }
+            
+
+        }
+    }
+}
