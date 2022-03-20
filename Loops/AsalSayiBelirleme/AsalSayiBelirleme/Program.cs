8e3deee60862cc4d14e8f7a4737a1607619eb69e
İrem ERGİN <92596689+iremergin@users.noreply.github.com>
2022-03-20 19:18:12 +0300

    Add files via upload

---
+++ Loops/AsalSayiBelirleme/AsalSayiBelirleme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AsalSayiBelirleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcının girdiği sayının asal olup olmama durumunu belirleme
+
+            Console.WriteLine("Enter Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            int counter = 0;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine(number +" "+"is a prime number :)");
+            }
+            else
+            {
+                Console.WriteLine(number + " " +"is not a prime number :(");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Loops/AsalSayiListeleme/AsalSayiListeleme/AsalSayiListeleme/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AsalSayiListeleme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ' den 10000 ' e kadar bütün asal sayıları listeleme
+
+            int counter = 0;
+
+            for (int i = 2; i < 10000; i++)
+            {
+                for (int k = 2; k < i; k++)
+                {
+                    if (i % k == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                counter = 0;
+            }
+        }
+    }
+}
