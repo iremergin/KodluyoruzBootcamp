7f54f2ab46b975b2bfa2563ded2e53745042e498
İrem ERGİN <92596689+iremergin@users.noreply.github.com>
2022-03-30 14:45:38 +0300

    Add files via upload

---
+++ ListCity/ListCity/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Koleksiyonda bulunan elemanların tekrarsız bir şekilde ekrana yazdırılmasını sağlayan program.
+
+            List<string> cities = new List<string>() { "Ankara", "Ankara", "Ankara", "Sinop", "İstanbul", "İstanbul", "Eskişehir", "Ankara" };
+
+            //Distinct Metodu -> Koleksiyonda bulunan elemanların tekrarsız bir şekilde ekrana yazdırılmasını sağlar.
+
+            List<string> listCity = cities.Distinct().ToList();
+
+            foreach (string city in listCity)
+            {
+                Console.WriteLine(city);
+            }
+
+        }
+    }
+}
